77d6c2550dc9826e20a9010e25538e7003e10ac3
Philipp von Weitershausen <philipp@weitershausen.de>
2007-06-04 12:14:36 +0000

    initial project structure

---
+++ setup.py
@@ -0,0 +1,34 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for the Acquisition egg package
+"""
+import os
+from setuptools import setup, find_packages, Extension
+
+setup(name='DateTime',
+      version = '2.11.0a1',
+      url='http://svn.zope.org/DateTime',
+      license='ZPL 2.1',
+      description='',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description='',
+      
+	  packages=find_packages('src'),
+	  package_dir={'': 'src'},
+
+      install_requires=['zope.interface'],
+      include_package_data=True,
+      zip_safe=False,
+      )
