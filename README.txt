67684648735c432abd6cf6d84380e3640241ac28
Philipp von Weitershausen <philipp@weitershausen.de>
2008-01-06 19:10:26 +0000

    Refer to src/DateTime/DateTime.txt for the long description (PyPI frontpage).

---
+++ README.txt
@@ -0,0 +1,1 @@
+Please refer to src/DateTime/DateTime.txt.
--- setup.py
+++ setup.py
@@ -20,16 +20,17 @@ setup(name='DateTime',
       version = '2.11.0dev',
       url='http://cheeseshop.python.org/pypi/DateTime',
       license='ZPL 2.1',
-      description='DateTime data type',
-      author='Zope Corporation and Contributors',
-      author_email='zope-dev@zope.org',
-      long_description="""\
+      description="""\
 This package provides a DateTime data type, as known from Zope 2.
 Unless you need to communicate with Zope 2 APIs, you're probably
 better off using Python's bult-in datetime module.""",
-      
-	  packages=find_packages('src'),
-	  package_dir={'': 'src'},
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=open(
+          os.path.join('src', 'DateTime', 'DateTime.txt')).read(),
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
 
       install_requires=['zope.interface',
                         'pytz',
