3249c4cfd81f7a078baeaf854c6ea13c49aa167b
Tres Seaver <tseaver@palladion.com>
2008-08-05 13:32:34 +0000

    Tag 2.11.1 release.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='DateTime',
-      version = '2.11.0b1',
+      version = '2.11.1',
       url='http://cheeseshop.python.org/pypi/DateTime',
       license='ZPL 2.1',
       description="""\
