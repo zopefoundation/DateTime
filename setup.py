fdb12f53d186f604e1de02a80cba570923109df8
Hanno Schlichting <hanno@hannosch.eu>
2009-02-02 20:50:15 +0000

    Update version after release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='DateTime',
-      version = '2.11.2',
+      version = '2.11.3dev',
       url='http://pypi.python.org/pypi/DateTime',
       license='ZPL 2.1',
       description="""\
