0f1cfef69f20544530a87f2df1476caeff042812
Hanno Schlichting <hanno@hannosch.eu>
2012-10-21 19:42:04 +0000

    vb

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='DateTime',
-      version = '3.0.2',
+      version = '3.0.3dev',
       url='http://pypi.python.org/pypi/DateTime',
       license='ZPL 2.1',
       description="""\
